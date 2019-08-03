47749bea94eec5a7a245e685eabfe854dd462a5a
pppscn <35696959@qq.com>
2019-08-03 21:56:57 +0800

    update php version

--- include/main.sh
+++ include/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DB_Info=('MySQL 5.1.73' 'MySQL 5.5.62' 'MySQL 5.6.44' 'MySQL 5.7.26' 'MySQL 8.0.13' 'MariaDB 5.5.63' 'MariaDB 10.0.38' 'MariaDB 10.1.40' 'MariaDB 10.2.24' 'MariaDB 10.3.15')
-PHP_Info=('PHP 5.2.17' 'PHP 5.3.29' 'PHP 5.4.45' 'PHP 5.5.38' 'PHP 5.6.40' 'PHP 7.0.33' 'PHP 7.1.30' 'PHP 7.2.20' 'PHP 7.3.7')
+PHP_Info=('PHP 5.2.17' 'PHP 5.3.29' 'PHP 5.4.45' 'PHP 5.5.38' 'PHP 5.6.40' 'PHP 7.0.33' 'PHP 7.1.31' 'PHP 7.2.21' 'PHP 7.3.8')
 Apache_Info=('Apache 2.2.34' 'Apache 2.4.39')
 
 Database_Selection()
