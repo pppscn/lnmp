2b2e76615edf41c72caffcbc5c7fbe109174b50d
pppscn <35696959@qq.com>
2020-12-13 15:38:14 +0800

    improve & update version

--- include/main.sh
+++ include/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DB_Info=('MySQL 5.1.73' 'MySQL 5.5.62' 'MySQL 5.6.48' 'MySQL 5.7.30' 'MySQL 8.0.20' 'MariaDB 5.5.68' 'MariaDB 10.1.45' 'MariaDB 10.2.32' 'MariaDB 10.3.23' 'MariaDB 10.4.13')
-PHP_Info=('PHP 5.2.17' 'PHP 5.3.29' 'PHP 5.4.45' 'PHP 5.5.38' 'PHP 5.6.40' 'PHP 7.0.33' 'PHP 7.1.33' 'PHP 7.2.34' 'PHP 7.3.24' 'PHP 7.4.12')
+PHP_Info=('PHP 5.2.17' 'PHP 5.3.29' 'PHP 5.4.45' 'PHP 5.5.38' 'PHP 5.6.40' 'PHP 7.0.33' 'PHP 7.1.33' 'PHP 7.2.34' 'PHP 7.3.25' 'PHP 7.4.13')
 Apache_Info=('Apache 2.2.34' 'Apache 2.4.46')
 
 Database_Selection()
@@ -775,5 +775,16 @@ TempMycnf_Clean()
     fi
     if [ -s /tmp/.mysql.tmp ]; then
         rm -f /tmp/.mysql.tmp
+    fi
+}
+
+StartOrStop()
+{
+    local action=$1
+    local service=$2
+    if command -v systemctl >/dev/null 2>&1 && [[ -s /etc/systemd/system/${service}.service || -s /lib/systemd/system/${service}.service || -s /usr/lib/systemd/system/${service}.service ]]; then
+        systemctl ${action} ${service}.service
+    else
+        /etc/init.d/${service} ${action}
     fi
 }
